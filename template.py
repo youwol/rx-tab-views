f8bd94370624b8e6d9d0bfc5a5b2cf98f5a08cf0
youwol-guillaume <greinisch@youwol.com>
2023-11-20 11:02:10 +0100

    ⬆️ `template.py` => upgrade `rxjs#^7` & `rx-vdom#^1.0.1`

--- template.py
+++ template.py
@@ -10,8 +10,8 @@ folder_path = Path(__file__).parent
 pkg_json = parse_json(folder_path / 'package.json')
 
 load_dependencies = {
-    "rxjs": "^6.5.5",
-    "@youwol/flux-view": "^1.0.3"
+    "rxjs": "^7.5.6",
+    "@youwol/rx-vdom": "^1.0.1"
 }
 
 template = Template(
@@ -41,8 +41,15 @@ shutil.copyfile(
     src=folder_path / '.template' / 'src' / 'auto-generated.ts',
     dst=folder_path / 'src' / 'auto-generated.ts'
 )
-for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'webpack.config.ts']:
+for file in [
+    'README.md',
+    '.gitignore',
+    '.npmignore',
+    '.prettierignore',
+    'LICENSE',
+    'package.json',
+    # 'tsconfig.json', needs to reference `rx-vdom-config.ts`
+    'webpack.config.ts']:
     shutil.copyfile(
         src=folder_path / '.template' / file,
         dst=folder_path / file
